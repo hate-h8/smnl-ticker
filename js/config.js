1e322ded2a05b1a5e9c1022fbee4e2db4f7d3d9b
hate-h8 <davidaycock2020@gmail.com>
2026-09-09 20:27:16 -0500

    fixing color again

--- js/config.js
+++ js/config.js
@@ -22,7 +22,7 @@ const THEME_META = [
 const DEFAULT_THEME = {
   "--headline-bg-top": "#12233B",
   "--headline-bg-bottom": "#12233B",
-  "--box-bg": "#BA1C07",
+  "--box-bg": "#921606",
   "--accent": "#E2B22C",
   "--text": "#FFFFFF",
   "--greenscreen": "#00FF00",
