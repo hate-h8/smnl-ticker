b51ed007c556668dfeec9bfd1ebe432de6f642f3
hate-h8 <davidaycock2020@gmail.com>
2026-09-09 20:16:53 -0500

    edited defualt color

--- js/config.js
+++ js/config.js
@@ -22,7 +22,7 @@ const THEME_META = [
 const DEFAULT_THEME = {
   "--headline-bg-top": "#12233B",
   "--headline-bg-bottom": "#12233B",
-  "--box-bg": "#FE285B",
+  "--box-bg": "#ba1c07",
   "--accent": "#E2B22C",
   "--text": "#FFFFFF",
   "--greenscreen": "#00FF00",
